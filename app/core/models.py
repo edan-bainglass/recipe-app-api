68b499b6942c62dd7fd9e91f7e60b10c51e92523
simplogic86 <edan.bainglass@gmail.com>
2022-06-27 10:40:16 -0500

    Conformed to flake8 E701

--- app/core/models.py
+++ app/core/models.py
@@ -15,7 +15,9 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save, and return a new user."""
-        if not email: raise ValueError('User must have an email address.')
+        if not email:
+            raise ValueError('User must have an email address.')
+
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
@@ -24,7 +26,9 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, email, password):
         """Create, save, and return a new superuser."""
-        if not email: raise ValueError('Superuser must have an email address.')
+        if not email:
+            raise ValueError('Superuser must have an email address.')
+
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
