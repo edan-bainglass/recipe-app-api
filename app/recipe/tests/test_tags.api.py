1862e8ef902fbf7d83836765bde3fefb438c99f2
simplogic86 <edan.bainglass@gmail.com>
2022-06-28 12:10:49 -0500

    Added tag delete feature and tests

--- app/recipe/tests/test_tags.api.py
+++ app/recipe/tests/test_tags.api.py
@@ -85,3 +85,13 @@ class PrivateTagsAPITests(TestCase):
         tag.refresh_from_db()
 
         self.assertEqual(tag.name, payload['name'])
+
+    def test_delete_tag(self):
+        """Test deleting a tag."""
+        tag = Tag.objects.create(user=self.user, name='Breakfast')
+
+        url = detail_url(tag.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Tag.objects.filter(user=self.user).exists())
