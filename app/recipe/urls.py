f3330dde94f356f39c7fa2ea3da7b122f8a5406d
simplogic86 <edan.bainglass@gmail.com>
2022-06-28 11:59:39 -0500

    Connected tag view url

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -12,6 +12,7 @@ from recipe import views
 # ModelViewSet class (CRUD)
 router = DefaultRouter()
 router.register('recipes', views.RecipeViewSet)
+router.register('tags', views.TagViewSet)
 
 app_name = 'recipe'
 
